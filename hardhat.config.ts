9d23a3a27e1fefbe89c332adbfade44c897cbb1e
alexey <alexnagornnyy.an@gmail.com>
2024-01-11 14:36:50 +0000

    🫡 updaterdhardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+if (process.env.PRIVATE_KEY == null) {
+  throw new Error("No PRIVATE_KEY in .env file")
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
-  // networks: {
-  //   ArbitrumSepolia: {
-  //     url: "https://sepolia-rollup.arbitrum.io/rpc",
-  //     accounts: [process.env.PRIVATE_KEY!]
-  //   }
-  // }
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    arbitrumSepolia: {
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      chainId: 421614,
+      accounts: [process.env.PRIVATE_KEY!]
+    },
+  },
+
 };
 
 export default config;
